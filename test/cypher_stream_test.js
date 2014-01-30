8aa0e529cb3675c99c05718d1b393cd467c75bac
Brian Gates <brian.g.gates@gmail.com>
2014-01-30 11:26:35 -0800

    Mock neo4j request for test.

--- test/cypher_stream_test.js
+++ test/cypher_stream_test.js
@@ -1,8 +1,20 @@
 var neo4j_cypher_stream = require('../index');
 var should              = require('should');
-var cypher              = neo4j_cypher_stream('http://localhost:7474/db/data/cypher');
+var cypher              = neo4j_cypher_stream('http://localhost:9000/db/data/cypher');
+var http                = require('http');
+var fs                  = require('fs');
 
 describe('Cypher stream', function () {
+  var server;
+  before(function(){
+    server = http.createServer(function (req, res) {
+      // simulate neo4j server
+      fs.createReadStream('./test/mock/neo4j_response.json').pipe(res);
+    }).listen(9000, 'localhost');
+  });
+  after(function(){
+    server.close();
+  })
 
   it('it works', function (done) {
     var results = 0;
