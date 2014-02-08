09da2a7228d084af21949c2b0169dcdc87bf7768
Brian Gates <brian.g.gates@gmail.com>
2014-02-07 23:09:29 -0800

    Significant rewrite to utilize oboe js in order to parse more easily.  Now with error handling.

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-var request                 = require('request');
-var Neo4jStreamDeserializer = require('neo4j-stream-deserializer');
+var request   = require('request');
+var oboe      = require('oboe');
+var Readable  = require('stream').Readable;
+var util      = require('util');
 
-module.exports = function(url) {
-  return function(query, params) {
-    var form = { query: query };
-    if(params) {
-      form.params = params;
-    }
-    return request.post({
-      url     : url,
-      form    : form,
-      headers : { "X-Stream": true, "Accept": "application/json" }
-    }).pipe(new Neo4jStreamDeserializer());
+util.inherits(CypherStream, Readable);
+
+function CypherStream (url, query, params) {
+  Readable.call(this, { objectMode: true });
+  var columns;
+  var stream = this;
+  oboe({
+    url     : url+'/db/data/cypher',
+    method  : 'POST',
+    headers : { "X-Stream": true, "Accept": "application/json" },
+    body    : { query: query, params: params  }
+  })
+  .node('!columns', function (c) {
+    stream.emit('columns', c);
+    columns = c;
+    this.forget();
+  })
+  .node('!data[*]', function (result, path, ancestors) {
+    var data = {};
+    columns.forEach(function (column, i) {
+      data[column] = result[i].data
+    });
+    stream.push(data)
+  })
+  .done(function (complete) {
+    stream.push(null);
+  })
+  .fail(function(error) {
+    stream.emit('error', error);
+    stream.push(null);
+  });
+
+  this._read = function () { }
+  return this;
+};
+
+module.exports = function Connection(url) {
+  return function CypherStreamFactory (query, params) {
+    return new CypherStream(url, query, params);
   }
 };
--- test/cypher_stream_test.js
+++ test/cypher_stream_test.js
@@ -1,30 +1,45 @@
-var neo4j_cypher_stream = require('../index');
 var should              = require('should');
-var cypher              = neo4j_cypher_stream('http://localhost:9000/db/data/cypher');
+var cypher              = require('../index')('http://localhost:7474');
 var http                = require('http');
 var fs                  = require('fs');
 
 describe('Cypher stream', function () {
-  var server;
-  before(function(){
-    server = http.createServer(function (req, res) {
-      // simulate neo4j server
-      fs.createReadStream('./test/mock/neo4j_response.json').pipe(res);
-    }).listen(9000, 'localhost');
+  before(function (done){
+    cypher('FOREACH (x IN range(1,10) | CREATE(:Test {test: true}))')
+      .on('end', done)
+      .resume();
   });
-  after(function(){
-    server.close();
-  })
+  after(function (done){
+    cypher('MATCH (n:Test) DELETE n')
+      .on('end', done)
+      .resume();
+  });
 
   it('it works', function (done) {
     var results = 0;
-    cypher('match (n {test: true}) return n limit 10')
+    cypher('match (n:Test {test: true}) return n limit 10')
       .on('data', function (result){
         results++;
         result.n.test.should.be.ok;
       })
       .on('end', function() {
         results.should.eql(10);
+        done();
+      })
+    ;
+  });
+
+  it('it handles errors', function (done) {
+    var error = false;
+    cypher('invalid query')
+      .on('data', function (data){
+        console.log(data);
+      })
+      .on('error', function (result){
+        error = true;
+      })
+      .on('end', function() {
+        error.should.be.ok;
         done();
       })
     ;
